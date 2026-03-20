0a9a3148c21855ced02c6dc42f4fccf6fa433ce7
mharvic <31197516+mharvic@users.noreply.github.com>
2026-03-20 14:33:43 -0600

    Create admin.js
    Added admin route

---
+++ routes/admin.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const authorize = require("../middleware/authorize");
+const authMiddleware = require("../middleware/auth");
+const router = express.Router();
+
+router.get("/protected", authMiddleware, authorize("admin"), (req, res) => {
+  res.status(200).json({ message: "Welcome admin user" });
+});
+
+module.exports = router;
